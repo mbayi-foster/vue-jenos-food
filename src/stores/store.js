49177e49729248599ba85f08a305cefc9ea8319a
Foster <mbayifoster@gmail.com>
2025-04-12 13:16:18 +0100

    ajout api Logout

--- src/stores/store.js
+++ src/stores/store.js
@@ -1,5 +1,5 @@
 import { defineStore } from 'pinia'
-
+import api from '../api/api'
 export const useUserStore = defineStore('user', {
   state: () => ({
     user: null,
@@ -9,13 +9,21 @@ export const useUserStore = defineStore('user', {
     login(userData) {
       this.user = userData
       this.isAuthenticated = true
-    
+
       localStorage.setItem('user', JSON.stringify(userData))
     },
-    logout() {
-      this.user = null
-      this.isAuthenticated = false
-      localStorage.removeItem('user')
+    async logout() {
+      try {
+        const data = await api.post('/logout/users');
+        this.user = null
+        this.isAuthenticated = false
+        localStorage.removeItem('user')
+      } catch (error) {
+
+      }
+
+
+
     },
     loadUser() {
       const userData = JSON.parse(localStorage.getItem('user'))
